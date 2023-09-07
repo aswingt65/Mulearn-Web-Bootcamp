1dc721ed893b61a8caf26a904dc0daa25d4a783c
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-09-07 10:20:20 +0530

    Create index.js

---
+++ fruit-sort/index.js
@@ -0,0 +1,15 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+function sortFruit() {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+            appleShelf.textContent += "🍎"
+        } else if (fruit[i] === "🍊") {
+            orangeShelf.textContent += "🍊"
+        }
+    }
+}
+
+sortFruit()
