4b12615ab7329855756182535e4d3273a6e1efe3
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-09-24 12:28:58 +0530

    Update Navbar.js

--- react-info/components/Navbar.js
+++ react-info/components/Navbar.js
@@ -1,10 +1,9 @@
 import React from "react"
-import image from "../react-info/images/react-icon-small.png"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src={image} className="nav--icon" />
+            <img src=url(https://raw.githubusercontent.com/aswingt65/Mulearn-Web-Bootcamp/main/react-info/images/react-icon-small.png) className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
