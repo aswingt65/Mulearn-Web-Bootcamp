191af9a39f55c34c1dc3adbdce21d0a0471f6fd4
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-09-30 09:24:22 +0530

    Delete airbnb-clone directory

--- airbnb-clone/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Navbar() {
-    return (
-        <nav>
-            <img src="../Mulearn-Web-Bootcamp/airbnb-clone/images/airbnb-logo.png" className="nav--logo" />
-        </nav>
-    )
-}
