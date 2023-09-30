d6d8f41813a1c4ed3a8d5dd782e195cfecfc7c5b
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-09-30 09:59:03 +0530

    Update Hero.js

--- airbnb-clone/components/Hero.js
+++ airbnb-clone/components/Hero.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Hero() {
     return (
         <section className="hero">
-            <img src="../blob/main/airbnb-clone/images/photo-grid.png" className="hero--photo" />
+            <img src="../airbnb-clone/images/photo-grid.png" className="hero--photo" />
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
