5044fee647b5d7757949208c8504c569da116c03
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-09-30 09:59:29 +0530

    Update Navbar.js

--- airbnb-clone/components/Navbar.js
+++ airbnb-clone/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../blob/main/airbnb-clone/images/airbnb-logo.png" className="nav--logo" />
+            <img src="../airbnb-clone/images/airbnb-logo.png" className="nav--logo" />
         </nav>
     )
 }
